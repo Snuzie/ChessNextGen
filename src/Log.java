aaeb58ebbd755b57dbf04ab597317eca260130ef
Mattias Lönnerberg <mattias.lonnerberg@gmail.com>
2013-05-02 15:47:35 +0200

    bättre logfil

--- src/Log.java
+++ src/Log.java
@@ -4,6 +4,9 @@ import javax.swing.*;
 public class Log extends JPanel {
 	private JList<String> list;
 	private DefaultListModel<String> listModel;
+	private int moveCount = 1;
+	private static String letters[] = new String[]
+			{"A","B","C","D","E","F","G","H"};
 	
 	public Log() {
 		super(new BorderLayout());
@@ -18,8 +21,16 @@ public class Log extends JPanel {
 	}
 	
 	public void addMove(Square from, Square to) {
-		String s1 = ""+from.getPos().getRow()+from.getPos().getColumn();
-		String s2 = ""+to.getPos().getRow()+to.getPos().getColumn();
-		listModel.addElement(s1 + "->" + s2);
+		//rows=letters, Cols=numbers
+		String s1 = "" + letters[from.getPos().getRow()]
+				+ (from.getPos().getColumn()+1);
+		String s2 = "" + letters[to.getPos().getRow()]
+				+ (to.getPos().getColumn()+1);
+		listModel.addElement(moveCount + ". " + s1 + "->" + s2);
+		moveCount++;
+	}
+	
+	public void gameOver() {
+		listModel.addElement("Game Over!");
 	}
 }
